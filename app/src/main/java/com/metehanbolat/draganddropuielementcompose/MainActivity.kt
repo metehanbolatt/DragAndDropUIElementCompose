5076e7c1fc200a0bc177081d448161e02d3cb264
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-17 19:01:59 +0300

    DragAndDrop functions

--- app/src/main/java/com/metehanbolat/draganddropuielementcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/draganddropuielementcompose/MainActivity.kt
@@ -3,6 +3,10 @@ package com.metehanbolat.draganddropuielementcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import com.metehanbolat.draganddropuielementcompose.ui.theme.DragAndDropUIElementComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +14,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DragAndDropUIElementComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
 
+                }
             }
         }
     }
